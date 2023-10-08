9cfd1ab1e9d1554283098eee6368f572495c4435
kayanbrandao <kayanbrandao.krb@gmail.com>
2023-10-08 19:06:40 -0400

    feat: creating the brand service

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.css']
+  styleUrls: ['./brand.component.css'],
 })
-export class BrandComponent {
-
-}
+export class BrandComponent {}
