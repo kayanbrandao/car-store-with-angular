a455644e8fedd15125d494c1a2ec1ef9705eea4a
kayanbrandao <kayanbrandao.krb@gmail.com>
2023-10-11 22:48:13 -0400

    refactor: adding the route to the model component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { BrandComponent } from './components/brand/brand.component';
+import { ModelComponent } from './components/model/model.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'brands', component: BrandComponent },
+  { path: 'models', component: ModelComponent },
 ];
 
 @NgModule({
